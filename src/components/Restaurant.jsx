8f317cfa63c3ba109c690c889f62f170d77a7396
joshuadelaney89 <73564714+joshuadelaney89@users.noreply.github.com>
2021-02-08 20:15:01 -0500

    joshua restaurant api push

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -2,38 +2,44 @@ import React, { useEffect, useState } from 'react';
 
 const Restaurant = (props) => {
     const [restaurant, setRestaurant] = useState([])
+    const [isLocal, setIsLocal] = useState(true)
 
     useEffect(() => {
-        getRestaurantList()
-    }, []);
+        if(props.geoLoc){
+            getRestaurantList()
+        }
+    }, [props.geoLoc, isLocal]);
+
 
     function getRestaurantList() {
         console.log(props.geoLoc.latitude, props.geoLoc.longitude);
+        let lat = props.geoLoc.latitude;
+        let lon = props.geoLoc.longitude;
 
-        let url = 'https://developers.zomato.com/api/v2.1/geocode?lat=40.1908087&lon=-85.3901047';
-        let key = 'd7e58923a65d5c41fab057b7dd3c4db6';
-        console.log(url);
+        let url = `https://developers.zomato.com/api/v2.1/geocode?lat=${lat}&lon=${lon}`;
+        let apiKey = 'd7e58923a65d5c41fab057b7dd3c4db6';
 
         fetch(url, {
             method: 'GET',
             headers: {
-                'user-key': key,
+                'user-key': apiKey,
                 'Content-Type': 'application/json',
-                'Accept': 'application/json'
+                'Accept': 'application/json',
             }
         })
         .then((res) => {
             return(res.json());
         })
         .then(json => {
-            console.log(json);
-            setRestaurant(json);
+            console.log(json.nearby_restaurants[0].restaurant.name);
+            setRestaurant(json.nearby_restaurants[0].restaurant.name);
         })
     }
 
+
     return (
         <div>
-            
+            <p>{restaurant}</p>
         </div>
     )
 }
